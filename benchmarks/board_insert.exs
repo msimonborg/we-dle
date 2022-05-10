94aa7d537d378eff50b6d691bf5ed2aa7c68d8ba
M. Simon Borg <msimonborg@gmail.com>
2022-05-10 17:50:16 -0400

    Benchmark and improve performance of Board.insert/3

--- benchmarks/board_insert.exs
+++ benchmarks/board_insert.exs
@@ -1,7 +1,8 @@
 defmodule NewBoard do
   defstruct [
     :word_length,
-    rows: {[], [], [], [], [], []}
+    rows: [[], [], [], [], [], []],
+    turns: 0
   ]
 
   def new(word_length)
@@ -11,103 +12,86 @@ defmodule NewBoard do
 
   def insert(%__MODULE__{} = board, guess, target)
       when is_binary(guess) and is_binary(target) do
-    with :ok <- validate_word_length(guess, board.word_length),
+    guess_graphs = String.graphemes(guess)
+    with :ok <- validate_word_length(guess_graphs, board.word_length),
          :ok <- validate_board_is_not_full(board) do
-      guess
-      |> compare(target)
+      target_graphs = String.graphemes(target)
+
+      guess_graphs
+      |> compare(target_graphs)
       |> insert(board)
     end
   end
 
-  defp insert(comparison, board) do
-    Enum.reduce_while(0..5, board, fn i, brd ->
-      if elem(brd.rows, i) == [] do
-        rows = put_elem(brd.rows, i, comparison)
-        {:halt, %{brd | rows: rows}}
-      else
-        {:cont, brd}
-      end
-    end)
+  defp insert(comparison, %{rows: rows, turns: turns} = board) do
+    %{
+      board |
+        rows: List.replace_at(rows, turns, comparison),
+        turns: turns + 1
+    }
   end
 
-  defp validate_word_length(guess, word_length) do
-    case length(to_charlist(guess)) == word_length do
-      true -> :ok
-      false -> {:error, :invalid_word_length}
-    end
-  end
+  defp validate_word_length(guess_graphs, word_length) when length(guess_graphs) == word_length,
+    do: :ok
 
-  defp validate_board_is_not_full(board) do
-    case elem(board.rows, 5) == [] do
-      true -> :ok
-      false -> {:error, :board_full}
-    end
-  end
+  defp validate_word_length(_, _), do: {:error, :invalid_word_length}
 
-  defp compare(guess, target) do
-    case guess == target do
+  defp validate_board_is_not_full(%{turns: turns}) when turns < 6, do: :ok
+  defp validate_board_is_not_full(%{turns: _turns}), do: {:error, :board_full}
+
+  defp compare(guess_graphs, target_graphs) do
+    case guess_graphs == target_graphs do
       true ->
-        guess
-        |> to_charlist()
-        |> Enum.map(&{0, to_string([&1])})
+        Enum.map(guess_graphs, &{0, &1})
 
       false ->
-        compare_letters(guess, target)
+        compare_letters(guess_graphs, target_graphs)
     end
   end
 
-  defp compare_letters(guess, target) do
-    target_chars = to_charlist(target)
-    guess_chars = to_charlist(guess)
-    max_index = length(guess_chars) - 1
+  defp compare_letters(guess_graphs, target_graphs) do
+    max_index = length(guess_graphs) - 1
 
-    guess_chars
-    |> compare_exact_matches(target_chars, max_index)
-    |> compare_possible_matches(target_chars, max_index)
+    guess_graphs
+    |> compare_exact_matches(target_graphs, max_index)
+    |> compare_possible_matches(target_graphs, max_index)
     |> Map.get(:comps)
   end
 
-  defp compare_exact_matches(guess_chars, target_chars, max_index) do
+  defp compare_exact_matches(guess_graphs, target_graphs, max_index) do
     Enum.reduce(0..max_index, %{distro: %{}, comps: []}, fn i, acc ->
-      char = Enum.at(guess_chars, i)
-      target_char = Enum.at(target_chars, i)
+      graph = Enum.at(guess_graphs, i)
+      target_graph = Enum.at(target_graphs, i)
 
-      if char == target_char do
-        comp = {0, to_string([char])}
-        comps = List.insert_at(acc.comps, -1, comp)
-        distro = Map.update(acc.distro, char, 1, &(&1 + 1))
+      if graph == target_graph do
+        comps = List.insert_at(acc.comps, -1, {0, graph})
+        distro = Map.update(acc.distro, graph, 1, &(&1 + 1))
         %{acc | distro: distro, comps: comps}
       else
-        comp = {:cont, char}
-        comps = List.insert_at(acc.comps, -1, comp)
+        comps = List.insert_at(acc.comps, -1, {:cont, graph})
         %{acc | comps: comps}
       end
     end)
   end
 
-  defp compare_possible_matches(first_pass, target_chars, max_index) do
-    target_distro =
-      Enum.reduce(target_chars, %{}, fn char, acc ->
-        Map.update(acc, char, 1, &(&1 + 1))
-      end)
+  defp compare_possible_matches(first_pass, target_graphs, max_index) do
+    target_distro = Enum.frequencies(target_graphs)
 
     Enum.reduce(0..max_index, first_pass, fn i, %{distro: distro, comps: comps} = acc ->
       case Enum.at(comps, i) do
-        {:cont, char} -> compare_possible_match(acc, i, comps, char, distro, target_distro)
-        {char, _} when is_integer(char) -> acc
+        {:cont, graph} -> compare_possible_match(acc, i, comps, graph, distro, target_distro)
+        {_, _} -> acc
       end
     end)
   end
 
-  defp compare_possible_match(acc, i, comps, char, distro, target_distro) do
-    if Map.get(distro, char, 0) < Map.get(target_distro, char, 0) do
-      comp = {1, to_string([char])}
-      comps = List.replace_at(comps, i, comp)
-      distro = Map.update(distro, char, 1, &(&1 + 1))
+  defp compare_possible_match(acc, i, comps, graph, distro, target_distro) do
+    if Map.get(distro, graph, 0) < Map.get(target_distro, graph, 0) do
+      comps = List.replace_at(comps, i, {1, graph})
+      distro = Map.update(distro, graph, 1, &(&1 + 1))
       %{acc | distro: distro, comps: comps}
     else
-      comp = {2, to_string([char])}
-      comps = List.replace_at(comps, i, comp)
+      comps = List.replace_at(comps, i, {2, graph})
       %{acc | comps: comps}
     end
   end
@@ -261,5 +245,58 @@ Benchee.run(
     "old_board" => old_board_insert
   },
   time: 10,
-  memory_time: 2
+  memory_time: 2,
+  reduction_time: 2,
+  formatters: [
+    Benchee.Formatters.HTML,
+    Benchee.Formatters.Console
+  ]
 )
+
+# Operating System: macOS
+# CPU Information: Intel(R) Core(TM) i7-4771 CPU @ 3.50GHz
+# Number of Available Cores: 8
+# Available memory: 32 GB
+# Elixir 1.13.4
+# Erlang 24.3.4
+
+# Benchmark suite executing with the following configuration:
+# warmup: 2 s
+# time: 10 s
+# memory time: 2 s
+# reduction time: 2 s
+# parallel: 1
+# inputs: none specified
+# Estimated total run time: 32 s
+
+# Benchmarking new_board ...
+# Benchmarking old_board ...
+# Generated benchmarks/output/results.html
+# Generated benchmarks/output/results_comparison.html
+# Generated benchmarks/output/results_new_board.html
+# Generated benchmarks/output/results_old_board.html
+# Opened report using open
+
+# Name                ips        average  deviation         median         99th %
+# new_board       66.98 K       14.93 μs    ±93.15%       13.66 μs       42.36 μs
+# old_board       52.45 K       19.07 μs   ±153.40%       14.75 μs       75.75 μs
+
+# Comparison:
+# new_board       66.98 K
+# old_board       52.45 K - 1.28x slower +4.14 μs
+
+# Memory usage statistics:
+
+# Name         Memory usage
+# new_board        19.63 KB
+# old_board        18.70 KB - 0.95x memory usage -0.92188 KB
+
+# **All measurements for memory usage were the same**
+
+# Reduction count statistics:
+
+# Name      Reduction count
+# new_board          1.95 K
+# old_board          2.52 K - 1.30x reduction count +0.57 K
+
+# **All measurements for reduction count were the same**
--- lib/we_dle/game/board.ex
+++ lib/we_dle/game/board.ex
@@ -6,14 +6,16 @@ defmodule WeDle.Game.Board do
 
   defstruct [
     :word_length,
-    rows: [[], [], [], [], [], []]
+    rows: [[], [], [], [], [], []],
+    turns: 0
   ]
 
   @type row_entry :: {non_neg_integer, String.t()}
   @type row :: [row_entry]
   @type t :: %__MODULE__{
           word_length: pos_integer,
-          rows: [row]
+          rows: [row],
+          turns: 0 | 1 | 2 | 3 | 4 | 5 | 6
         }
 
   @doc """
@@ -60,117 +62,100 @@ defmodule WeDle.Game.Board do
       iex> WeDle.Game.Board.insert(board, "hello", "world")
       %WeDle.Game.Board{
         rows: [[{2, "h"}, {2, "e"}, {2, "l"}, {0, "l"}, {1, "o"}], [], [], [], [], []],
-        word_length: 5
+        word_length: 5,
+        turns: 1
       }
 
       iex> WeDle.Game.Board.new(5) |> WeDle.Game.Board.insert("hello", "hello")
       %WeDle.Game.Board{
         rows: [[{0, "h"}, {0, "e"}, {0, "l"}, {0, "l"}, {0, "o"}], [], [], [], [], []],
-        word_length: 5
+        word_length: 5,
+        turns: 1
       }
   """
   def insert(%__MODULE__{} = board, guess, target)
       when is_binary(guess) and is_binary(target) do
-    with :ok <- validate_word_length(guess, board.word_length),
+    guess_graphs = String.graphemes(guess)
+
+    with :ok <- validate_word_length(guess_graphs, board.word_length),
          :ok <- validate_board_is_not_full(board) do
-      guess
-      |> compare(target)
+      target_graphs = String.graphemes(target)
+
+      guess_graphs
+      |> compare(target_graphs)
       |> insert(board)
     end
   end
 
-  defp insert(comparison, board) do
-    Enum.reduce_while(0..5, board, fn i, brd ->
-      if Enum.at(brd.rows, i) == [] do
-        rows = List.replace_at(brd.rows, i, comparison)
-        {:halt, %{brd | rows: rows}}
-      else
-        {:cont, brd}
-      end
-    end)
+  defp insert(comparison, %{rows: rows, turns: turns} = board) do
+    %{
+      board
+      | rows: List.replace_at(rows, turns, comparison),
+        turns: turns + 1
+    }
   end
 
-  defp validate_word_length(guess, word_length) do
-    case length(to_charlist(guess)) == word_length do
-      true -> :ok
-      false -> {:error, :invalid_word_length}
-    end
-  end
+  defp validate_word_length(guess_graphs, word_length) when length(guess_graphs) == word_length,
+    do: :ok
 
-  defp validate_board_is_not_full(board) do
-    board
-    |> Map.get(:rows)
-    |> Enum.any?(&(&1 == []))
-    |> case do
-      true -> :ok
-      false -> {:error, :board_full}
-    end
-  end
+  defp validate_word_length(_, _), do: {:error, :invalid_word_length}
 
-  defp compare(guess, target) do
-    case guess == target do
+  defp validate_board_is_not_full(%{turns: turns}) when turns < 6, do: :ok
+  defp validate_board_is_not_full(%{turns: _turns}), do: {:error, :board_full}
+
+  defp compare(guess_graphs, target_graphs) do
+    case guess_graphs == target_graphs do
       true ->
-        guess
-        |> to_charlist()
-        |> Enum.map(&{0, to_string([&1])})
+        Enum.map(guess_graphs, &{0, &1})
 
       false ->
-        compare_letters(guess, target)
+        compare_letters(guess_graphs, target_graphs)
     end
   end
 
-  defp compare_letters(guess, target) do
-    target_chars = to_charlist(target)
-    guess_chars = to_charlist(guess)
-    max_index = length(guess_chars) - 1
+  defp compare_letters(guess_graphs, target_graphs) do
+    max_index = length(guess_graphs) - 1
 
-    guess_chars
-    |> compare_exact_matches(target_chars, max_index)
-    |> compare_possible_matches(target_chars, max_index)
+    guess_graphs
+    |> compare_exact_matches(target_graphs, max_index)
+    |> compare_possible_matches(target_graphs, max_index)
     |> Map.get(:comps)
   end
 
-  defp compare_exact_matches(guess_chars, target_chars, max_index) do
+  defp compare_exact_matches(guess_graphs, target_graphs, max_index) do
     Enum.reduce(0..max_index, %{distro: %{}, comps: []}, fn i, acc ->
-      char = Enum.at(guess_chars, i)
-      target_char = Enum.at(target_chars, i)
+      graph = Enum.at(guess_graphs, i)
+      target_graph = Enum.at(target_graphs, i)
 
-      if char == target_char do
-        comp = {0, to_string([char])}
-        comps = List.insert_at(acc.comps, -1, comp)
-        distro = Map.update(acc.distro, char, 1, &(&1 + 1))
+      if graph == target_graph do
+        comps = List.insert_at(acc.comps, -1, {0, graph})
+        distro = Map.update(acc.distro, graph, 1, &(&1 + 1))
         %{acc | distro: distro, comps: comps}
       else
-        comp = {:cont, char}
-        comps = List.insert_at(acc.comps, -1, comp)
+        comps = List.insert_at(acc.comps, -1, {:cont, graph})
         %{acc | comps: comps}
       end
     end)
   end
 
-  defp compare_possible_matches(first_pass, target_chars, max_index) do
-    target_distro =
-      Enum.reduce(target_chars, %{}, fn char, acc ->
-        Map.update(acc, char, 1, &(&1 + 1))
-      end)
+  defp compare_possible_matches(first_pass, target_graphs, max_index) do
+    target_distro = Enum.frequencies(target_graphs)
 
     Enum.reduce(0..max_index, first_pass, fn i, %{distro: distro, comps: comps} = acc ->
       case Enum.at(comps, i) do
-        {:cont, char} -> compare_possible_match(acc, i, comps, char, distro, target_distro)
-        {char, _} when is_integer(char) -> acc
+        {:cont, graph} -> compare_possible_match(acc, i, comps, graph, distro, target_distro)
+        {_, _} -> acc
       end
     end)
   end
 
-  defp compare_possible_match(acc, i, comps, char, distro, target_distro) do
-    if Map.get(distro, char, 0) < Map.get(target_distro, char, 0) do
-      comp = {1, to_string([char])}
-      comps = List.replace_at(comps, i, comp)
-      distro = Map.update(distro, char, 1, &(&1 + 1))
+  defp compare_possible_match(acc, i, comps, graph, distro, target_distro) do
+    if Map.get(distro, graph, 0) < Map.get(target_distro, graph, 0) do
+      comps = List.replace_at(comps, i, {1, graph})
+      distro = Map.update(distro, graph, 1, &(&1 + 1))
       %{acc | distro: distro, comps: comps}
     else
-      comp = {2, to_string([char])}
-      comps = List.replace_at(comps, i, comp)
+      comps = List.replace_at(comps, i, {2, graph})
       %{acc | comps: comps}
     end
   end

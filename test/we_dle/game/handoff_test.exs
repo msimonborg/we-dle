828a6ab89cee60008b0ead4a4db94a33c1cfb7df
M. Simon Borg <msimonborg@gmail.com>
2022-05-27 08:04:30 -0400

    eliminate errors in handoff_test

--- test/we_dle/game/handoff_test.exs
+++ test/we_dle/game/handoff_test.exs
@@ -1,114 +1,115 @@
-# defmodule WeDle.Game.HandoffTest do
-#   use ExUnit.Case
+defmodule WeDle.Game.HandoffTest do
+  use ExUnit.Case, async: false
 
-#   import WeDle.Game
+  import WeDle.Game
 
-#   alias Ecto.Adapters.SQL.Sandbox
-#   alias WeDle.{Game, Game.Board, Game.Player, Handoffs, Repo}
+  alias WeDle.{Game, Game.Board, Game.Player, Handoffs}
 
-#   setup_all do
-#     on_exit(fn ->
-#       WeDle.Handoffs.delete_all_handoffs()
-#     end)
-#   end
+  setup do
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(WeDle.Repo, shared: true, sandbox: false)
 
-#   describe "game state handoff" do
-#     test "a game can handoff its state at shutdown to a new instance" do
-#       Sandbox.unboxed_run(Repo, fn ->
-#         game = "handoff1"
+    on_exit(fn ->
+      WeDle.Handoffs.delete_all_handoffs()
+      Ecto.Adapters.SQL.Sandbox.stop_owner(pid)
+    end)
 
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
+    :ok
+  end
 
-#         {:ok, %Player{challenge: "hello"}} = set_challenge(game, "p1", "hello")
-#         {:ok, %Player{challenge: "world"}} = set_challenge(game, "p2", "world")
+  describe "game state handoff" do
+    test "a game can handoff its state at shutdown to a new instance" do
+      game = "handoff1"
 
-#         game_pid = whereis(game)
-#         assert is_pid(game_pid)
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
 
-#         Process.exit(game_pid, :shutdown)
+      {:ok, %Player{challenge: "hello"}} = set_challenge(game, "p1", "hello")
+      {:ok, %Player{challenge: "world"}} = set_challenge(game, "p2", "world")
 
-#         Process.sleep(10)
+      game_pid = whereis(game)
+      assert is_pid(game_pid)
 
-#         assert whereis(game) |> is_nil()
+      Process.exit(game_pid, :shutdown)
 
-#         assert {:ok, %Player{challenge: "hello"}} = start_or_join(game, "p1")
-#         assert {:ok, %Player{challenge: "world"}} = start_or_join(game, "p2")
+      Process.sleep(10)
 
-#         assert whereis(game) |> is_pid()
-#         assert Handoffs.get_handoff(game) |> is_nil()
-#       end)
-#     end
+      assert whereis(game) |> is_nil()
 
-#     test "a game will receive a newly synced state handoff after the game has started" do
-#       Sandbox.unboxed_run(Repo, fn ->
-#         game = "handoff2"
+      assert {:ok, %Player{challenge: "hello"}} = start_or_join(game, "p1")
+      assert {:ok, %Player{challenge: "world"}} = start_or_join(game, "p2")
 
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
+      assert whereis(game) |> is_pid()
+      assert Handoffs.get_handoff(game) |> is_nil()
+    end
 
-#         handoff_state = %Game{
-#           edge_servers: %{},
-#           id: game,
-#           players: %{
-#             "p1" => %Player{
-#               board: %Board{
-#                 rows: [[], [], [], [], [], []],
-#                 solved: false,
-#                 turns: 0,
-#                 word_length: 5
-#               },
-#               challenge: "hello",
-#               game_id: game,
-#               id: "p1"
-#             },
-#             "p2" => %Player{
-#               board: %Board{
-#                 rows: [[], [], [], [], [], []],
-#                 solved: false,
-#                 turns: 0,
-#                 word_length: 5
-#               },
-#               challenge: "world",
-#               game_id: game,
-#               id: "p2"
-#             }
-#           },
-#           winner: nil,
-#           word_length: 5
-#         }
+    test "a game will receive a newly synced state handoff after the game has started" do
+      game = "handoff2"
 
-#         Handoffs.create_handoff(handoff_state)
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
 
-#         Process.sleep(1000)
+      handoff_state = %Game{
+        edge_servers: %{},
+        id: game,
+        players: %{
+          "p1" => %Player{
+            board: %Board{
+              rows: [[], [], [], [], [], []],
+              solved: false,
+              turns: 0,
+              word_length: 5
+            },
+            challenge: "hello",
+            game_id: game,
+            id: "p1"
+          },
+          "p2" => %Player{
+            board: %Board{
+              rows: [[], [], [], [], [], []],
+              solved: false,
+              turns: 0,
+              word_length: 5
+            },
+            challenge: "world",
+            game_id: game,
+            id: "p2"
+          }
+        },
+        winner: nil,
+        word_length: 5
+      }
 
-#         assert {:ok, %Player{challenge: "hello"}} = start_or_join(game, "p1")
-#         assert {:ok, %Player{challenge: "world"}} = start_or_join(game, "p2")
-#       end)
-#     end
+      Handoffs.create_handoff(handoff_state)
 
-#     test "game does not handoff state when exit reason is normal" do
-#       Sandbox.unboxed_run(Repo, fn ->
-#         game = "handoff3"
+      # Wait for handoff to be delivered
+      Process.sleep(50)
 
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
-#         {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
+      assert {:ok, %Player{challenge: "hello"}} = start_or_join(game, "p1")
+      assert {:ok, %Player{challenge: "world"}} = start_or_join(game, "p2")
 
-#         {:ok, %Player{challenge: "hello"}} = set_challenge(game, "p1", "hello")
-#         {:ok, %Player{challenge: "world"}} = set_challenge(game, "p2", "world")
+      game |> whereis() |> Process.exit(:normal)
+    end
 
-#         game_pid = whereis(game)
-#         assert is_pid(game_pid)
+    test "game does not handoff state when exit reason is normal" do
+      game = "handoff3"
 
-#         Process.exit(game_pid, :normal)
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
+      {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
 
-#         Process.sleep(10)
+      {:ok, %Player{challenge: "hello"}} = set_challenge(game, "p1", "hello")
+      {:ok, %Player{challenge: "world"}} = set_challenge(game, "p2", "world")
 
-#         assert whereis(game) |> is_nil()
+      game_pid = whereis(game)
+      assert is_pid(game_pid)
 
-#         assert {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
-#         assert {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
-#       end)
-#     end
-#   end
-# end
+      Process.exit(game_pid, :normal)
+
+      Process.sleep(10)
+
+      assert whereis(game) |> is_nil()
+
+      assert {:ok, %Player{challenge: nil}} = start_or_join(game, "p1")
+      assert {:ok, %Player{challenge: nil}} = start_or_join(game, "p2")
+    end
+  end
+end

d09799cd8dc099a15d2d555a982df65db0de2f6f
M. Simon Borg <msimonborg@gmail.com>
2022-05-28 03:46:03 -0400

    update WordleWords tests

--- test/we_dle/wordle_words_test.exs
+++ test/we_dle/wordle_words_test.exs
@@ -16,17 +16,21 @@ defmodule WeDle.WordleWordsTest do
     |> List.flatten(answers())
   end
 
-  test "answers_contains?/1 validates words correctly" do
-    for word <- answers() do
-      assert answers_contains?(word)
-      refute answers_contains?(word <> "!")
-    end
+  test "todays_answer?/1 validates words correctly" do
+    map =
+      for word <- answers(), reduce: %{} do
+        acc -> Map.put(acc, word, todays_answer?(word))
+      end
+
+    assert Enum.count(map, fn {_word, bool} -> not bool end) == map_size(map) - 1
+    assert Enum.count(map, fn {_word, bool} -> bool end) == 1
+    assert Map.get(map, todays_answer()) == true
   end
 
-  test "allowed_contains?/1 validates words correctly" do
+  test "allowed?/1 validates words correctly" do
     for word <- allowed() do
-      assert allowed_contains?(word)
-      refute allowed_contains?(word <> "!")
+      assert allowed?(word)
+      refute allowed?(word <> "!")
     end
   end
 end

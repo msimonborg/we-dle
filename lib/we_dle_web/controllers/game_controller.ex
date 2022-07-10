0903b1ecd25df6b4ef2f703f4d7672917264b47d
M. Simon Borg <msimonborg@gmail.com>
2022-07-10 02:11:35 -0400

    add game_mode param to controller actions

--- lib/we_dle_web/controllers/game_controller.ex
+++ lib/we_dle_web/controllers/game_controller.ex
@@ -13,25 +13,30 @@ defmodule WeDleWeb.GameController do
 
   def lobby(conn, _params) do
     game_id = get_session(conn, :game_id)
+    game_mode = get_session(conn, :game_mode)
 
-    if game_id do
-      redirect(conn, to: Routes.game_path(conn, :game, game_id))
+    if game_id && game_mode do
+      redirect(conn, to: Routes.game_path(conn, :game, game_id, %{game_mode: game_mode}))
     else
       live_render(conn, LobbyLive)
     end
   end
 
-  def game(conn, %{"game_id" => game_id}) do
+  def game(conn, %{"game_id" => game_id, "game_mode" => game_mode}) do
     if Game.exists?(game_id) do
       conn
       |> put_session(:game_id, game_id)
+      |> put_session(:game_mode, game_mode)
       |> live_render(GameLive)
     else
       conn
       |> delete_session(:game_id)
+      |> delete_session(:game_mode)
       |> redirect(to: Routes.game_path(conn, :lobby))
     end
   end
+
+  def game(conn, _), do: redirect(conn, to: Routes.game_path(conn, :lobby))
 
   defp remove_app_layout(conn, _), do: put_layout(conn, false)
 end
